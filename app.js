01ffc497a6431fdd7682fdf0d4ea1a0fe2bfd71a
dhanush K <dhanushkrishna2402@icloud.com>
2026-01-09 21:23:58 +0530

    learning interfaces today

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 "use strict";
-var a;
-a = 12;
-a = "harsh";
-if (typeof (a) === "string") {
-    a.toUpperCase;
+// var a: unknown;
+// a = 12;
+function func(obj) {
+    obj.name = "name";
+    obj.email = "email";
+    obj.password = "pass";
 }
--- app.ts
+++ app.ts
@@ -1,8 +1,26 @@
-var a: unknown;
-a = 12;
+// var a: unknown;
+// a = 12;
 
-a = "harsh";
+// a = "harsh";
 
-if (typeof(a) === "string") {
-    a.toUpperCase; 
+// if (typeof(a) === "string") {
+//     a.toUpperCase; 
+// }
+
+
+interface User{
+    name: string;
+    email: string;
+    password: string;
+    dob?: number;
+}
+
+interface Admin extends User{
+    admin: boolean;
+}
+
+function func(obj:User) {
+    obj.name = "name";
+    obj.email = "email";
+    obj.password = "pass";
 }
